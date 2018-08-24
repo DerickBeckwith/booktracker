6ddf38e4ba0345d848246afab4f0c2fd45290fdf
Derick Beckwith <derick.beckwith@gmail.com>
2018-08-24 11:08:09 -0400

    initial commit

---
+++ src/app/core/data.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+
+import { allBooks, allReaders } from 'app/data';
+import { LoggerService } from './logger.service';
+import { Reader } from "app/models/reader";
+import { Book } from "app/models/book";
+import { BookTrackerError } from 'app/models/bookTrackerError';
+
+@Injectable()
+export class DataService {
+
+  constructor(private loggerService: LoggerService) { }
+
+  mostPopularBook: Book = allBooks[0];
+
+  setMostPopularBook(popularBook: Book): void {
+    this.mostPopularBook = popularBook;
+  }
+
+  getAllReaders(): Reader[] {
+    return allReaders;
+  }
+
+  getReaderById(id: number): Reader {
+    return allReaders.find(reader => reader.readerID === id);
+  }
+
+  getAllBooks(): Book[] {
+    return allBooks;
+  }
+
+  getBookById(id: number): Book {
+    return allBooks.find(book => book.bookID === id);
+  }  
+}
